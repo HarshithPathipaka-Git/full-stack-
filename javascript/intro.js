500bbb78f82044562c366041d709fef4b3170d58
Harshith Pathipaka <harshithkumarpathipaka@gmail.com>
2026-06-06 10:12:52 +0530

    lesson 1 : Variables

---
+++ javascript/intro.js
@@ -0,0 +1,30 @@
+// Variable 
+// variables are containers for storing data values
+
+let name = "Harshith ";
+let age = 18 ;
+let favtech = "full stack development";
+let dream = "Be independent ";
+
+// print statement 
+
+console.log(name);
+console.log(age);
+console.log(favtech);
+console.log(dream);
+
+// key word used to save variables is let and var and const
+
+// let vs const 
+
+let age1 = 19;
+age1 = 18 ;
+console.log(age1);
+
+const country = "india";
+// country = "usa"; // this will give error because we cannot change the value of a constant variable
+console.log(country);
+
+
+// use const by default .
+// use let when valuie need to change....
